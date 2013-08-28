60ac8669cfd552282d2f577fe88faf9a44b39631
Laurent Chedanne <laurent@chedanne.pro>
2013-08-28 17:53:56 +0200

    Home page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+        	new Oxygen\FrameworkBundle\OxygenFrameworkBundle(),
+        	new Oxygen\UIBundle\OxygenUIBundle(),
             new Soletic\OxygenWebsiteBundle\SoleticOxygenWebsiteBundle(),
+        	
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
